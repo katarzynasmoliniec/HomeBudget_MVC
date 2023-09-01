5417c93b839e98471d2ccf38aab438b67b76142a
Katarzyna Smoliniec <katarzyna.smoliniec.programista@gmail.com>
2023-09-01 16:06:00 +0200

    Add Model-User to project with MVC

---
+++ Core/View.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Core;
+
+class View
+{
+
+    /**
+     * Render a view file
+     *
+     * @param string $view  The view file
+     * @param array $args  Associative array of data to display in the view (optional)
+     *
+     * @return void
+     */
+    public static function render($view, $args = [])
+    {
+        extract($args, EXTR_SKIP);
+
+        $file ="../App/Views/$view";
+
+        if (is_readable($file)) {
+            require $file;
+        } else {
+            throw new \Exception("{$file} not found");
+        }
+    }
+
+    /**
+     * Render a view template using Twig
+     *
+     * @param string $template  The template file
+     * @param array $args  Associative array of data to display in the view (optional)
+     *
+     * @return void
+     */
+
+    public static function renderTemplate($template, $args = [])
+    {
+        echo static::getTemplate($template, $args = []);
+    }
+
+    public static function getTemplate($template, $args = [])
+    {
+        static $twig = null;
+
+        if ($twig === null) {
+            $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/App/Views');
+            $twig = new \Twig\Environment($loader, [
+                'debug' => true
+            ]);
+        }
+
+        return $twig->render($template, $args);
+    }
+}
