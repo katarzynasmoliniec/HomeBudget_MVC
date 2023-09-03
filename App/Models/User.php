b079d9182e1c09605326b2b07936f1e1c10a343c
Katarzyna Smoliniec <katarzyna.smoliniec.programista@gmail.com>
2023-09-03 15:23:13 +0200

    "JS add to singup"

--- App/Models/User.php
+++ App/Models/User.php
@@ -4,21 +4,15 @@ namespace App\Models;
 
 use PDO;
 
-/**
- * Example user model
- *
- * PHP version 7.0
- */
 class User extends \Core\Model
 {
+  public $errors = [];
+  public $name;
+  public $email;
+  public $password;
+  public $password_confirmation;
+  
 
-  /**
-   * Class constructor
-   *
-   * @param array $data  Initial property values
-   *
-   * @return void
-   */
   public function __construct($data)
   {
     foreach ($data as $key => $value) {
@@ -26,25 +20,72 @@ class User extends \Core\Model
     };
   }
 
-  /**
-   * Save the user model with the current property values
-   *
-   * @return void
-   */
   public function save()
-  {
-    $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
+    {
+        $this->validate();
 
-    $sql = 'INSERT INTO users (name, email, password_hash)
-            VALUES (:name, :email, :password_hash)';
+        if (empty($this->errors)) {
 
-    $db = static::getDB();
-    $stmt = $db->prepare($sql);
+            $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
 
-    $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
-    $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
-    $stmt->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+            $sql = 'INSERT INTO users (name, email, password_hash)
+                    VALUES (:name, :email, :password_hash)';
+                                              
+            $db = static::getDB();
+            $stmt = $db->prepare($sql);
+                                                  
+            $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
+            $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
+            $stmt->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+                                          
+            return $stmt->execute();
+        }
 
-    $stmt->execute();
-  }
+        return false;
+    }
+ public function validate()
+    {
+        // Name
+        if ($this->name == '') {
+            $this->errors[] = 'Name is required';
+        }
+
+        // email address
+        if (filter_var($this->email, FILTER_VALIDATE_EMAIL) === false) {
+            $this->errors[] = 'Invalid email';
+        }
+        if ($this->emailExists($this->email)) {
+            $this->errors[] = 'email already taken';
+        }
+
+        // Password
+        if ($this->password != $this->password_confirmation) {
+            $this->errors[] = 'Password must match confirmation';
+        }
+
+        if (strlen($this->password) < 6) {
+            $this->errors[] = 'Please enter at least 6 characters for the password';
+        }
+
+        if (preg_match('/.*[a-z]+.*/i', $this->password) == 0) {
+            $this->errors[] = 'Password needs at least one letter';
+        }
+
+        if (preg_match('/.*\d+.*/i', $this->password) == 0) {
+            $this->errors[] = 'Password needs at least one number';
+        }
+    }
+
+ protected function emailExists($email)
+    {
+        $sql = 'SELECT * FROM users WHERE email = :email';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        return $stmt->fetch() !== false;
+    }
 }
--- Core/View.php
+++ Core/View.php
@@ -2,6 +2,7 @@
 
 namespace Core;
 
+#[\AllowDynamicProperties]
 class View
 {
 
